2fd1429ac2ab2c2152ae9f6de2aa75947d4de9b2
robertoderosa <roberto.derosa96@libero.it>
2022-11-28 18:07:03 +0100

    commit default auth

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/welcome', function () {
-    return view('welcome test');
-});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
